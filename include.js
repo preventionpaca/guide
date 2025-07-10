8bf45015df55c70c626bdc5e3a55012cee26292c
preventionpaca <rudy.themines@gmail.com>
2025-07-10 05:02:52 +0200

    h

--- include.js
+++ include.js
@@ -2,24 +2,14 @@
 function include(file, elementId) {
   fetch(file)
     .then(response => response.text())
-    .then(html => {
-      document.getElementById(elementId).innerHTML = html;
+    .then(html => document.getElementById(elementId).innerHTML = html)
+    .then(() => {
       if (elementId === "header-container") {
         const breadcrumb = document.getElementById("breadcrumb");
-        if (!breadcrumb) return;
-        const filename = window.location.pathname.split('/').pop().replace('.html', '');
-        const pageName = {
-          "index": "Accueil",
-          "guide": "Guide",
-          "reglementation": "Réglementation",
-          "base-paca": "Base PACA",
-          "edition": "Édition de documents",
-          "contact": "Contact"
-        }[filename] || filename;
-        if (filename !== "index") {
-          breadcrumb.innerHTML = '<a href="index.html">Accueil</a> &gt; ' + pageName;
-        } else {
-          breadcrumb.innerHTML = 'Accueil';
+        const path = window.location.pathname.split("/").pop().replace(".html", "");
+        const title = document.title || path;
+        if (breadcrumb && path !== "index") {
+          breadcrumb.innerHTML = '<a href="index.html">Accueil</a> &gt; ' + title;
         }
       }
     });
