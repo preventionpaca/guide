905a114641e0397b84686fcfdaca320ebb765914
rt <rudy.themines@gmail.com>
2025-12-16 22:14:33 +0100

    Update pp-config.js

--- js/pp-config.js
+++ js/pp-config.js
@@ -12,8 +12,8 @@
 
   // Liens (UN SEUL endroit à maintenir)
   window.PP_LINKS = {
-    home:   "https://preventionpaca.github.io/guide/index.html",
-    portal: "https://preventionpaca.github.io/guide/portail.html",
+    home:   "https://preventionpaca.github.io/guide/",
+    portal: "https://preventionpaca.github.io/guide/",
     admin:  "https://preventionpaca.github.io/guide/guide_iframe.html#https://docs.getgrist.com/gvPEJV3qAHS9/Equipements-de-travail-et-produits/p/75?embed=true&style=singlePage&exclude-headers=true",
     ddfpt:  "https://preventionpaca.github.io/guide/guide_iframe.html#https://docs.getgrist.com/gvPEJV3qAHS9/Equipements-de-travail-et-produits/p/74?embed=true&style=singlePage&exclude-headers=true"
   };
