965c4b6fb31c32c10aec05066e33745400bfd07c
rt <rudy.themines@gmail.com>
2026-01-31 15:55:40 +0100

    Update pp-runtime.js

--- js/pp-runtime.js
+++ js/pp-runtime.js
@@ -1,7 +1,6 @@
 export function isInGristWidget() {
-  return !!(
-    window.grist &&
-    window.grist.docApi &&
-    window.parent !== window
-  );
+  return !!(window.grist && window.grist.docApi && window.parent !== window);
 }
+
+// BONUS: on l'expose aussi en global pour les scripts non-module
+window.isInGristWidget = isInGristWidget;
