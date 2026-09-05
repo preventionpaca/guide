8482b2c98f02f8e489cc6132490307cb6196283f
Zemyns <rudy.themines@gmail.com>
2026-09-05 17:33:09 +0200

    DEV.156 - stabilisation configuration preview

---
+++ preview-config.js
@@ -0,0 +1,4 @@
+/* Configuration publique dédiée au mode recette. Ne contient aucun jeton. */
+window.PP_PREVIEW_CONFIG = Object.freeze({
+  tokenSha256: "359658a33e6795b3b0da4e8decc6b6bee70ddb1e526fd2b3339841ca44b42ef8"
+});
--- site-config.js
+++ site-config.js
@@ -3,7 +3,6 @@ window.PP_SITE_CONFIG = Object.freeze({
   maintenance: true,
   environment: "DEV",
   appUrl: "https://script.google.com/macros/s/AKfycbxp1WsKD41zQUphVPNJ5MfcjO6eN6NjGEkBpxB9D-2QW-mZplS1j0j4i4p7YGn7oTLE/exec",
-  previewTokenSha256: "359658a33e6795b3b0da4e8decc6b6bee70ddb1e526fd2b3339841ca44b42ef8",
   gristHost: "https://camin.getgrist.com",
   externalGristDocuments: Object.freeze({
     functionalDiagram: Object.freeze({
