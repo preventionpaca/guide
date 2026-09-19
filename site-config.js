d2a00436fa4e2190c96c4851f7c5d45551308bd1
Zemyns <rudy.themines@gmail.com>
2026-09-19 11:55:27 +0200

    Maintenance - domaines et formations post-EV166

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 /* Configuration publique non sensible du routeur Prévention PACA. */
 window.PP_SITE_CONFIG = Object.freeze({
-  maintenance: false,
+  maintenance: true,
   environment: "DEV",
   appUrl: "https://script.google.com/macros/s/AKfycbxp1WsKD41zQUphVPNJ5MfcjO6eN6NjGEkBpxB9D-2QW-mZplS1j0j4i4p7YGn7oTLE/exec",
   gristHost: "https://camin.getgrist.com",
